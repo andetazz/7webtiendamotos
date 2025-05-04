a3b0135b62bd89d62710f4592e32a62a40233e22
andetazz <andeazz87@gmail.com>
2025-05-04 17:14:46 -0500

    first commit211

--- run.py
+++ run.py
@@ -3,9 +3,8 @@ import os
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-port = int(os.environ.get("PORT", 5000))
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
